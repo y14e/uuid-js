d54f6fde4ed2d1dda82082306465b6156dbe3897
Yusuke Kamiyamane <y@y14e.com>
2025-04-11 22:42:48 +0900

    20250411a

--- uuid.js
+++ uuid.js
@@ -1,8 +1,6 @@
-function generateUUID() {
+export function generateUUID() {
   if (typeof crypto.randomUUID === 'function') return crypto.randomUUID();
   const uuid = URL.createObjectURL(new Blob()).slice(-36);
   URL.revokeObjectURL(uuid);
   return uuid;
 }
-
-export { generateUUID };
--- uuid.ts
+++ uuid.ts
@@ -1,8 +1,6 @@
-function generateUUID(): string {
+export function generateUUID(): string {
   if (typeof crypto.randomUUID === 'function') return crypto.randomUUID();
   const uuid = URL.createObjectURL(new Blob()).slice(-36);
   URL.revokeObjectURL(uuid);
   return uuid;
 }
-
-export { generateUUID };
